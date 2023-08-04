540c0558ff1cb70565c9050c6d2ae43e9e786c55
Egor Panevin <predsedatel228@yandex.ru>
2023-08-04 20:47:35 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  number = String(number);
+  let h = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+  let t = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let d = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let s = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+  if (number.length === 1) {
+    return s[number]
+  }
+  if (number.length === 2) {
+    if (number < 20) {
+      return t[number - 10]
+    }
+    if (number >= 20) {
+      if (number%10 === 0) {
+        return d[number/10-2]
+      }
+      else return d[Math.floor(number / 10 - 2)] + ' ' + s[number % 10]
+    }
+  }
+  if (number.length === 3) {
+    if (number%100 === 0) {
+      return h[Math.floor(number / 100 - 1)]
+    }
+    if (0 < number%100 && number%100 <= 9) {
+      return h[Math.floor(number / 100 - 1)] + ' ' + s[number % 10]
+    }
+    if (9 < number%100 && number%100 < 20) {
+      return h[Math.floor(number / 100 - 1)] + ' ' + t[number%100-10]
+    }
+    if (number % 100 >= 20) {
+      if (number%10 !==0){
+        return h[Math.floor(number / 100 - 1)] + ' ' + d[number[1] - 2] + ' ' + s[number % 10]
+      }
+      if (number%10 === 0) {
+        return h[Math.floor(number / 100 - 1)] + ' ' + d[number[1] - 2]
+      }
+    }
+  }
 }
